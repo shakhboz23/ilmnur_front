51575d3e11a0502ee3d373aa8bdd7dd2472449d3
shakhboz23 <upwork02092005@gmail.com>
2025-05-25 16:24:03 +0500

    .

---
+++ .output/public/_nuxt/sZSVDDoh.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./Cbg_HfJW.js";import{ac as c,a1 as r}from"./BwpkegjB.js";const o={};function t(n,a){return r(),c("div")}const f=e(o,[["render",t]]);export{f as default};
--- pages/shorts/index.vue
+++ pages/shorts/index.vue
@@ -7,9 +7,10 @@
                 depth: 100,
                 modifier: 1,
                 slideShadows: true,
-            }" :spaceBetween="120" :modules="modules" :mousewheel="true">
+            }" :simulateTouch="true"
+            touch-angle="1" touch-ratio="45" :spaceBetween="120" :modules="modules" :mousewheel="true">
             <swiper-slide :id="i.id" v-for="i in useLessons.store.lessons" :key="i.id"
-                class="video-container mx-auto md:max-w-[450px] max-w-[80%] shadow-2xl w-[60%] relative">
+                class="video-container mx-auto md:max-w-[450px] max-w-[70%] shadow-2xl w-[60%] relative">
                 <ClientOnly>
                     <VideoReader v-if="i.video" class="h-[calc(100vh_-_160px)] overflow-hidden" :url="i.video" />
                     <EditorTiptapEditor class="overflow-hidden video-container overflow-y-auto" :modelValue="i?.content"
