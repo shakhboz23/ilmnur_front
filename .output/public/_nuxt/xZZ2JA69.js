05f2121e773a048382769e25272a370e9aa76cfa
shakhboz23 <upwork02092005@gmail.com>
2025-05-26 09:35:45 +0500

    responsive short for mobile

--- .output/public/_nuxt/xZZ2JA69.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./CADBH1JM.js";import{ac as c,a1 as n}from"./BwpkegjB.js";const o={};function r(t,a){return n(),c("div",null," developing... ")}const f=e(o,[["render",r]]);export{f as default};
--- pages/shorts/index.vue
+++ pages/shorts/index.vue
@@ -9,55 +9,49 @@
                 slideShadows: true,
             }" :simulateTouch="true" :spaceBetween="120" :modules="modules" :mousewheel="true">
             <swiper-slide :id="i.id" v-for="i in useLessons.store.lessons" :key="i.id"
-                class="video-container mx-auto md:max-w-[450px] max-w-[70%] shadow-2xl w-[60%] relative">
-                <ClientOnly>
-                    <VideoReader v-if="i.video" class="h-[calc(100vh_-_160px)] overflow-hidden" :url="i.video" />
-                    <EditorTiptapEditor class="overflow-hidden video-container overflow-y-auto" :modelValue="i?.content"
-                        :editable="false" />
-                </ClientOnly>
-                <ul
-                    class="flex flex-col justify-end items-end h-full text-center video-actions absolute top-0 right-[-80px] space-y-3">
-                    <li>
-                        <button class="p-4 bg_cf2 rounded-full" @click="like(video.id)">
-                            <img class="w-6 h-6" src="@/assets/svg/icon/star.svg" alt="">
-                        </button>
-                        <p>123</p>
-                    </li>
-                    <li>
-                        <button class="p-4 bg_cf2 rounded-full" @click="comment(video.id)">
-                            <img class="w-6 h-6" src="@/assets/svg/sidebar/chat.svg" alt="">
-                        </button>
-                        <p>123</p>
-                    </li>
-                    <li>
-                        <button class="p-4 bg_cf2 rounded-full" @click="comment(video.id)">
-                            <img class="w-6 h-6" src="@/assets/svg/icon/share.svg" alt="">
-                        </button>
-                        <p>share</p>
-                    </li>
-                    <li>
-                        <button class="p-4 bg_cf2 rounded-full" @click="comment(video.id)">
-                            <img class="w-6 h-6" src="@/assets/svg/icon/threedot.svg" alt="">
-                        </button>
-                    </li>
-                    <li>
-                        <button class="bg_cf2 rounded-full" @click="comment(video.id)">
-                            <UIAvatar class="min-h-[56px] min-w-[56px] max-h-[56px] max-w-[56px]" :src="i.image" />
-                        </button>
-                    </li>
-                </ul>
+                class="video-container mx-auto md:max-w-[450px]">
+                <div class="flex items-end">
+                    <div  class="w-full shadow-2xl">
+                        <ClientOnly>
+                            <VideoReader v-if="i.video" class="h-[calc(100vh_-_160px)] overflow-hidden"
+                                :url="i.video" />
+                            <EditorTiptapEditor class="overflow-hidden bg-white video-container overflow-y-auto"
+                                :modelValue="i?.content" :editable="false" />
+                        </ClientOnly>
+                    </div>
+                    <ul class="flex flex-col min-w-[60px] justify-end items-end h-full text-center space-y-3">
+                        <li>
+                            <button class="p-4 bg_cf2 rounded-full" @click="like(video.id)">
+                                <img class="w-6 h-6" src="@/assets/svg/icon/star.svg" alt="">
+                            </button>
+                            <p>123</p>
+                        </li>
+                        <li>
+                            <button class="p-4 bg_cf2 rounded-full" @click="comment(video.id)">
+                                <img class="w-6 h-6" src="@/assets/svg/sidebar/chat.svg" alt="">
+                            </button>
+                            <p>123</p>
+                        </li>
+                        <li>
+                            <button class="p-4 bg_cf2 rounded-full" @click="comment(video.id)">
+                                <img class="w-6 h-6" src="@/assets/svg/icon/share.svg" alt="">
+                            </button>
+                            <p>share</p>
+                        </li>
+                        <li>
+                            <button class="p-4 bg_cf2 rounded-full" @click="comment(video.id)">
+                                <img class="w-6 h-6" src="@/assets/svg/icon/threedot.svg" alt="">
+                            </button>
+                        </li>
+                        <li>
+                            <button class="bg_cf2 rounded-full" @click="comment(video.id)">
+                                <UIAvatar class="min-h-[56px] min-w-[56px] max-h-[56px] max-w-[56px]" :src="i.image" />
+                            </button>
+                        </li>
+                    </ul>
+                </div>
             </swiper-slide>
         </swiper>
-        <!-- <div :id="i.id" v-for="i in useLessons.store.lessons" :key="i.id"
-            class="video-container mx-auto p-5 max-w-[400px]">
-            <ClientOnly>
-                <VideoReader class="h-[calc(100vh_-_160px)] overflow-hidden" :url="i.video" />
-            </ClientOnly>
-            <div class="video-actions">
-                <button @click="like(video.id)">Like</button>
-                <button @click="comment(video.id)">Comment</button>
-            </div>
-        </div> -->
     </div>
 </template>
 
