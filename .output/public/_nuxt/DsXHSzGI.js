41b4a9840b83f1830a32d3903c6d4be80693c381
shakhboz23 <upwork02092005@gmail.com>
2025-06-01 10:11:18 +0500

    comment integrated with backend

---
+++ .output/public/_nuxt/DsXHSzGI.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./DCTQrESd.js";import{ac as c,a1 as n}from"./CbB-Q2Zp.js";const o={};function r(t,a){return n(),c("div",null," developing... ")}const f=e(o,[["render",r]]);export{f as default};
